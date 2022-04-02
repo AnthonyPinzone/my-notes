8c8e649ea396c559e733d195b3e1c1d5ba655950
AnthonyPinzone <the1andonlypz@gmail.com>
2022-04-02 07:31:15 -0400

    Added images with Gatsby Image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = () => {
   return (
@@ -7,6 +8,10 @@ const AboutPage = () => {
       <p>
         Hi there! I'm the proud creator of this site, which I built with Gatsby.
       </p>
+      <StaticImage
+        alt='All Pro Offensive Tackle Terron Armstead signed a mega deal with the Miami Dolphins'
+        src='../images/Terron-Armstead-Media.webp'
+      />
     </Layout>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        alt='Kansas City Chiefs Wide Receiver Tyreek Hill was Traded to the Miami Dolphins'
+        src='https://dolphinswire.usatoday.com/wp-content/uploads/sites/53/2022/03/Tyreek-Hill_TA.png?w=1000&h=600'
+      />
     </Layout>
   );
 };

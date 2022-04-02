8a42df2722bbaa5550683a22641a0786cd6d82be
AnthonyPinzone <the1andonlypz@gmail.com>
2022-04-02 17:51:40 -0400

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     // "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
     // "gatsby-plugin-react-helmet",
-    // "gatsby-plugin-mdx",
+    "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     // "gatsby-transformer-sharp",
     {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,36 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle='My Blog Posts'>
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>
+            {node.frontmatter.chapterNumber}. {node.frontmatter.chapterName}
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
-  query {
-    allFile {
+  query BlogQuery {
+    allMdx(sort: { order: ASC, fields: frontmatter___chapterNumber }) {
       nodes {
-        name
+        frontmatter {
+          book
+          chapterName
+          chapterNumber
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        body
       }
     }
   }
